9d2ef2996cea60ccaef5265da7ff2c7276397d36
Samir Patil <samirspatil742099@gmail.com>
2022-07-22 02:27:22 +0530

    [ADD] List and Detail View

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from gallery.views import ImageListView, ImageDetailView
 
 urlpatterns = [
+
+    path("images/", ImageListView.as_view(), name="images"),
+    path("images/<int:pk>", ImageDetailView.as_view(), name="image-detail"),
+    path("images/new", ImageDetailView.as_view(), name="image-detail"),
+
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from gallery.models import Image
 
-# Create your views here.
+
+class ImageListView(ListView):
+
+    model = Image
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context, "Context")
+        return context
+
+class ImageDetailView(DetailView):
+
+    model = Image
+    # context_object_name = 'image'
+    # queryset = Image.objects.all()
+    #
+
+    def get_object(self):
+        obj = super().get_object()
+        print("tsar")
+        return obj
