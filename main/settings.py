daee62f4b3bc8fe9d074a4c8007f0cef553df69d
Samir Patil <samirspatil742099@gmail.com>
2022-07-21 20:36:27 +0530

    [Change] Main module name changed

--- main/settings.py
+++ main/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'HackerEarth.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'HackerEarth.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Database
