b34f62a8fd4c849b625f1c8e155b9f474a31f16c
Samir Patil <samirspatil742099@gmail.com>
2022-07-23 12:49:17 +0530

    [CHANGE] ALLOWED_HOST

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q0*969f4crjww1)nigbceu!rn()g0*$5@luli64y4pq1njidk%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
